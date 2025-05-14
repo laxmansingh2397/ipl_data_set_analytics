0013f1715d5b36f835445da05f633834da1c5b21
Laxman Singh <singhlaxman2397@gmail.com>
2025-05-14 11:11:53 +0530

    Remove matches.py

--- matches.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('/home/laxman/python_virtual_env/my_env/ipl/required_data/matches.csv','r') as csvfile:
-    matches_data = csv.DictReader(csvfile)
-
-    for matches in matches_data:
-        print(matches)
